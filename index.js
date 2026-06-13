d9c7fb46ebf7c73ac36751f115f23ac394bbd450
Furkan Uzun <barisfurkanuz@gmail.com>
2026-06-13 14:41:01 +0300

    created

---
+++ index.js
@@ -0,0 +1,11 @@
+function cypressTestProjesi() {
+  const checklist = {
+    //github repo adresini gir: Örn: https://github.com/Workintech/hello-world
+    project_url: 'https://github.com/brsfurkanuzun/day7-4',
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
